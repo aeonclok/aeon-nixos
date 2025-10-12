3d4ef543bdd04cf9da50899b2a88e2525640d80a
reima <reima.kokko@valolink.fi>
2025-10-12 10:39:28 +0300

    nvf and hyprland basic conf

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-
 {
   description = "NixOS configuration with Home Manager";
 
@@ -6,18 +5,28 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nvf = {
+      url = "github:NotAShelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+
+  outputs = { nvf, self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
         home-manager.nixosModules.home-manager
         {
+	  home-manager.sharedModules = [
+            nvf.homeManagerModules.default
+          ];
+
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.reima = import ./home.nix;
+	  home-manager.backupFileExtension = "backup";
         }
       ];
     };
--- home.nix
+++ home.nix
@@ -6,4 +6,74 @@
   home.homeDirectory = "/home/reima";
   programs.home-manager.enable = true;
   home.stateVersion = "25.05";
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+
+    settings = {
+      input = {
+        kb_layout = "fi";
+	repeat_rate = 50;
+	repeat_delay = 300;
+	mouse_refocus = false;
+        touchpad = {
+          natural_scroll = true;  # set to false for classic direction
+        };
+      };
+
+      general = {
+        gaps_in = 2;
+        gaps_out = 2;
+        border_size = 0;
+        layout = "master";
+      };
+
+      decoration = {
+        rounding = 0;
+        shadow = {
+          range = 0;
+          render_power = 0;
+        };
+        blur = {
+          enabled = false;
+          size = 5;
+          passes = 2;
+        };
+      };
+
+      "$mod" = "Alt_L";
+      bind = [
+        "$mod, Q, exec, kitty"
+        "$mod, C, killactive"
+        "$mod, U, togglefloating"
+        "$mod, F, fullscreen"
+        "$mod, RETURN, exec, firefox"
+      ]
+            ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+        builtins.concatLists (builtins.genList (i:
+            let ws = i + 1;
+            in [
+              "$mod, code:1${toString i}, workspace, ${toString ws}"
+              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+            ]
+          )
+          9)
+      );
+      monitor = [
+        "eDP-1,1920x1080@60,0x0,1"
+      ];
+    };
+  };
+  programs.nvf = {
+    enable = true;
+    settings = {
+      vim.viAlias = false;
+      vim.vimAlias = true;
+      vim.lsp = {
+        enable = true;
+      };
+    };
+  };
 }
