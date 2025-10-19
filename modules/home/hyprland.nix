19409f60c5988d8022fc5c8275b4820e3b194384
reima <reima.kokko@valolink.fi>
2025-10-19 13:55:14 +0300

    disable hyprland wallpapaer

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -75,6 +75,11 @@
         # "eDP-1,1920x1080@60,0x0,1"
         ",preferred,auto,1"
       ];
+
+      misc = {
+        disable_splash_rendering = true;
+        disable_hyprland_logo    = true;
+      };
     };
   };
 
