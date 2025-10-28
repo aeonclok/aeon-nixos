89924dc7974bc232ec43f913f9f51e9562991fd1
Reima Kokko <reima.kokko@valolink.fi>
2025-10-28 23:03:34 +0200

    nvf to home for carbon

--- hosts/thinkpad-carbon/home.nix
+++ hosts/thinkpad-carbon/home.nix
@@ -11,6 +11,7 @@
     ../../modules/home/wezterm.nix
     ../../modules/home/hyprland.nix
     ../../modules/home/waybar.nix
+    ../../modules/home/nvf.nix
     ../../modules/home/tmux.nix
   ];
   
