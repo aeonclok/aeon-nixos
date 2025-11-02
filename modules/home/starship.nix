cb2f542dae548fba5aa71d7078a5fbca9e51b334
Reima Kokko <reima.kokko@valolink.fi>
2025-11-02 08:10:10 +0200

    starship

---
+++ modules/home/starship.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }: {
+  # home.packages = with pkgs; [ pkgs.starship ];
+
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true; # hooks starship into fish automatically
+  };
+  # Symlink ~/.tmux.conf → ~/nix/modules/home/tmux.conf
+  home.file.".config/starship.toml".source = config.lib.file.mkOutOfStoreSymlink
+    "/home/reima/nix/modules/home/starship.toml";
+}
