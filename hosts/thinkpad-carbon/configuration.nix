0c98b9c0778ab519a9b2c190d3a3c9fd96a8dcf2
Reima Kokko <reima.kokko@valolink.fi>
2026-04-26 13:15:18 +0300

    carbon suspend

--- hosts/thinkpad-carbon/configuration.nix
+++ hosts/thinkpad-carbon/configuration.nix
@@ -1,25 +1,49 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+{
   imports = [ ../../modules/system/base.nix ];
 
   networking.hostName = "thinkpad-carbon";
   system.stateVersion = "25.05";
 
-  swapDevices = [{
-    device = "/var/lib/swapfile";
-    size = 16 * 1024; # Size in MB (16GB here)
-  }];
+  services.upower = {
+    enable = true;
+    percentageLow = 30;
+    percentageCritical = 20;
+    percentageAction = 15;
+    criticalPowerAction = "PowerOff";
+  };
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      USB_AUTOSUSPEND = 1;
+      START_CHARGE_THRESH_BAT0 = 75;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+    };
+  };
+
   boot.initrd.systemd.enable = true;
+
+  boot.resumeDevice = "/dev/disk/by-uuid/096e32fa-fb71-48c4-8790-55214e4b027c";
   boot.kernelParams = [
     "mem_sleep_default=deep"
-  ]; # Important for ThinkPads to sleep/resume correctly
+    "acpi_osi=Linux"
+    "resume_offset=12939264"
+  ];
+
+  services.logind.settings.Login.HandleLidSwitch = "suspend-then-hibernate";
+
+  systemd.sleep.extraConfig = ''
+    HibernateDelaySec=20m
+  '';
+
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 16 * 1024;
+    }
+  ];
   powerManagement.enable = true;
-  services.logind.settings.Login = {
-    lidSwitch = "suspend-then-hibernate";
-    extraConfig = ''
-      HibernateDelaySec=1m
-    '';
-  };
   services.resolved.enable = true;
   networking.networkmanager.dns = "systemd-resolved";
 }
-
